82b402548a53d7d6639e125a0c47be247ab0ab31
Rezvanova Darya <daria.rezv@gmail.com>
2018-10-12 19:14:42 +0600

    my first matrix

---
+++ workshop1/src/it/sevenbits/workshop/Main.java
@@ -0,0 +1,13 @@
+package it.sevenbits.workshop;
+
+import it.sevenbits.workshop.matrix.Matrix;
+
+public class Main {
+
+    public static void main(String[] args){
+        Matrix matrix = new Matrix(3, 4);
+        System.out.println(matrix.toString());
+        matrix.inverseMatrix();
+        System.out.println(matrix);
+    }
+}
