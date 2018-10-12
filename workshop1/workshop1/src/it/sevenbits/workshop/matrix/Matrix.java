640abe556a6f1f6b5fa45a29f5ca8a5176d6ff20
Rezvanova Darya <daria.rezv@gmail.com>
2018-10-12 19:13:08 +0600

    my first matrix

---
+++ workshop1/workshop1/src/it/sevenbits/workshop/matrix/Matrix.java
@@ -0,0 +1,46 @@
+package it.sevenbits.workshop.matrix;
+
+import java.util.Random;
+
+public class Matrix {
+
+    private final int CELL_MAX_VALUE = 100;
+    private Cell[][] matrix;
+
+    private Matrix(){
+    }
+
+    public Matrix(int rowsCount, int columsCount){
+        matrix = new Cell[rowsCount][columsCount];
+        fillMatrixByRandom();
+    }
+
+    private void fillMatrixByRandom(){
+        Random rand = new Random();
+        for (int x = 0; x < matrix.length; x++)
+            for (int y = 0; y < matrix[x].length; y++){
+                int cellValue = rand.nextInt(CELL_MAX_VALUE);
+                Cell newCell = new Cell(x,y, cellValue);
+                matrix[x][y] = newCell;
+            }
+    }
+
+    public void inverseMatrix(){
+        for (int x = 0; x < matrix.length; x++)
+            for (int y = 0; y < matrix[x].length; y++){
+                matrix[x][y].inverse();
+            }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int x = 0; x < matrix.length; x++) {
+            for (int y = 0; y < matrix[x].length; y++) {
+                sb.append(matrix[x][y].getValue()).append(" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
