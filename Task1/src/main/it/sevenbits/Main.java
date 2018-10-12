c7e8f70e5855502f2f08319ec379daf1bf9bc627
rezvd <32849199+rezvd@users.noreply.github.com>
2018-10-13 02:50:00 +0600

    Added finding the biggest lengh

--- Task1/src/main/it/sevenbits/Main.java
+++ Task1/src/main/it/sevenbits/Main.java
@@ -3,12 +3,11 @@ package main.it.sevenbits;
 import java.util.Random;
 
 public class Main {
-    public Main() {
-    }
 
     public static void main(String[] arg) {
         Line[] lines = new Line[5];
         Random r = new Random();
+        double max = Double.MIN_VALUE;
 
         for(int i = 0; i < 5; ++i) {
             int startX = r.nextInt(100) - 50;
@@ -16,9 +15,13 @@ public class Main {
             int endX = r.nextInt(100) - 50;
             int endY = r.nextInt(100) - 50;
             lines[i] = new Line(new Point(startX, startY), new Point(endX, endY));
+            if (lines[i].getLength() > max){
+                max = lines[i].getLength();
+            }
             System.out.println("Line " + (i + 1) + ": start(" + startX + ", " + startY + "), end(" + endX + ", " + endY + ");");
             System.out.println("Length of this line is " + lines[i].getLength());
         }
+        System.out.println("The biggest length is " + max);
 
     }
 }
